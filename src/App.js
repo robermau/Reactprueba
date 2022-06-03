7cfe51d9c6b8373a13f336777ad4a8492815c3f8
robermau <robertom.perez@outlook.com>
2022-06-02 21:18:29 -0300

    first version

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/App.css';
+import Main from './components/Main';
+
+function App() {
+    return ( <div className ="App">
+
+      <h1>Texto de prueba!!!!</h1>
+        <Main></Main>
+        <Main></Main>
+        <Main></Main>
+        <Main></Main>
+       </div>
+    );
+}
+
+export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from   'react'
+
+function Main(){
+
+    return(
+        <h1>Texto de prueba</h1>
+    )
+}
+
+export default Main
